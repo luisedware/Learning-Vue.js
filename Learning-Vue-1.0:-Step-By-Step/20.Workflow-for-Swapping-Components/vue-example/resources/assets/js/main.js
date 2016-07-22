9f1942f9e3d4ea7138417a7608c034a46a3d2e35
luisedware <luisedware@qq.com>
2016-07-22 11:33:33 +0800

    Learning Vue.js

---
+++ Learning-Vue-1.0:-Step-By-Step/20.Workflow-for-Swapping-Components/vue-example/resources/assets/js/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+
+import HomeView from './components/HomeView.vue';
+
+
+new Vue({
+    el: 'body',
+    components: {
+        HomeView
+    }
+    // <home-view></home-view>
+    // <component is="home-view"></component>
+});
---
+++ Learning-Vue-1.0:-Step-By-Step/20.Workflow-for-Swapping-Components/vue-example/resources/views/layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="zh">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<component is="home-view">
+		@yield('content')
+	</component>
+	<script type="text/javascript" src="/js/main.js"></script>
+</body>
+</html>
+
